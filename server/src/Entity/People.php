c7d3b95bd1138d8b71f5eeaadf8eacdca1f4ccda
Neo-Diamons <romain.hure@epitech.eu>
2024-01-16 00:53:56 +0100

    feat(server): working progress on symfony (never use that again).

---
+++ server/src/Entity/People.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PeopleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="users", schema="trombiDB")
+ */
+#[ORM\Entity(repositoryClass: PeopleRepository::class)]
+class Peoples
+{
+    #[ORM\Id]
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $job = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $equip = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $agency = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photo_fun_url = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photo_pro_url = null;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getJob(): ?string
+    {
+        return $this->job;
+    }
+
+    public function setJob(string $job): static
+    {
+        $this->job = $job;
+
+        return $this;
+    }
+
+    public function getEquip(): ?string
+    {
+        return $this->equip;
+    }
+
+    public function setEquip(string $equip): static
+    {
+        $this->equip = $equip;
+
+        return $this;
+    }
+
+    public function getAgency(): ?string
+    {
+        return $this->agency;
+    }
+
+    public function setAgency(string $agency): static
+    {
+        $this->agency = $agency;
+
+        return $this;
+    }
+
+    public function getPhotoFunUrl(): ?string
+    {
+        return $this->photo_fun_url;
+    }
+
+    public function setPhotoFunUrl(?string $photo_fun_url): static
+    {
+        $this->photo_fun_url = $photo_fun_url;
+
+        return $this;
+    }
+
+    public function getPhotoProUrl(): ?string
+    {
+        return $this->photo_pro_url;
+    }
+
+    public function setPhotoProUrl(?string $photo_pro_url): static
+    {
+        $this->photo_pro_url = $photo_pro_url;
+
+        return $this;
+    }
+}
---
+++ server/src/Repository/PeopleRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Peoples;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Peoples>
+ *
+ * @method Peoples|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Peoples|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Peoples[]    findAll()
+ * @method Peoples[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PeopleRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Peoples::class);
+    }
+
+
+   /**
+    * @return Peoples[] Returns an array of Peoples objects
+    */
+
+//    /**
+//     * @return Peoples[] Returns an array of Peoples objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Peoples
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
