c7a7b122a1a91cd04e46a7b92b2da903d76359da
Fenriir42 <raid3r2205@gmail.com>
2024-01-17 14:19:18 +0100

    style (Update): css for improved UI

--- clientapp/src/components/Header.jsx
+++ clientapp/src/components/Header.jsx
@@ -1,17 +1,17 @@
+// Header.jsx
+import React from 'react';
+import './Header.css';
+
 function Header() {
-    return (
-        <header className="
-            w-full h-[81px] p-[20px] bg-secondary
-            flex content-center 
-        ">
-            <img
-                loading="eager"
-                src="https://www.6tm.com/app/uploads/2023/09/6tm_title-001.svg"
-                alt="6TM logo with title" 
-                width="300" height="50"
-            />
-        </header>
-    );
+  return (
+    <header>
+      <img
+        loading="eager"
+        src="https://www.6tm.com/app/uploads/2023/09/6tm_title-001.svg"
+        alt="6TM logo with title"
+      />
+    </header>
+  );
 }
 
 export default Header;
--- clientapp/src/pages/login/login.jsx
+++ clientapp/src/pages/login/login.jsx
@@ -1,6 +1,8 @@
-// src/pages/login/Login.jsx
+// Login.jsx
 import React, { useState } from 'react';
-import './Login.css';
+import { Link } from 'react-router-dom';
+import Header from './../../components/Header'; // Update the path based on your project structure
+import './login.css';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -12,28 +14,43 @@ function Login() {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <h2>Login</h2>
-        <form>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="button" onClick={handleLogin}>
-            Login
-          </button>
-        </form>
+    <div className="login-page">
+      <Header />
+
+      <div className="login-container">
+        <div className="login-box">
+          <h2>Login</h2>
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="white-input"
+                placeholder="Nom d'utilisateur"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="white-input"
+                placeholder="Mot de passe"
+              />
+            </div>
+            <Link to="/forgot-password" className="forgot-password-link">
+              Utilisateur ou mot de passe oublié
+            </Link>
+            <br />
+            <br />
+            <button type="button" onClick={handleLogin} className="login-button">
+              Se connecter
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
