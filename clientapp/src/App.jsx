197ebc86c49b681939316553fa688af8b22ff6e6
Yann Morvan <yann.morvan@epitech.eu>
2024-01-17 23:02:18 +0000

    feat: svg for background

--- clientapp/src/App.jsx
+++ clientapp/src/App.jsx
@@ -2,7 +2,11 @@ import { Router } from "./Router";
 
 function App() {
     return (
-        <div className="App bg-[#0c1414] min-w-screen min-h-screen">
+        <div className="relative App bg-[#0c1414] min-w-screen min-h-screen">
+            {/* <div className="absolute top-10 right-20 z-[1] w-[30%] h-[30%] opacity-10" style={{ transform: "translate(-50%, 50%)" }}>
+                <img src="/svgexport-3.svg" alt="Description of the SVG" />
+                <object type="image/svg+xml" data="/svgexport-3.svg">Your browser does not support SVG</object>
+            </div> */}
             <Router />
         </div >
     );
