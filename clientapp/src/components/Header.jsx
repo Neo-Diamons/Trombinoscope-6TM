9b3d23c95dc4425a0b501cabb9c5bd321c356596
Fenriir42 <raid3r2205@gmail.com>
2024-01-17 17:17:20 +0100

    feat: make header logo clickable with redirect to home

--- clientapp/src/components/Header.jsx
+++ clientapp/src/components/Header.jsx
@@ -1,15 +1,17 @@
-// Header.jsx
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Header.css';
 
 function Header() {
   return (
     <header>
-      <img
-        loading="eager"
-        src="https://www.6tm.com/app/uploads/2023/09/6tm_title-001.svg"
-        alt="6TM logo with title"
-      />
+      <Link to="/">
+        <img
+          loading="eager"
+          src="https://www.6tm.com/app/uploads/2023/09/6tm_title-001.svg"
+          alt="6TM logo with title"
+        />
+      </Link>
     </header>
   );
 }
