e93a6c74f9bc1b5b8def08f50f13425444007e2d
Fenriir42 <raid3r2205@gmail.com>
2024-01-18 16:42:01 +0100

    feat(scrollbar): add a custom scrollbar for chrome

--- clientapp/src/App.jsx
+++ clientapp/src/App.jsx
@@ -1,26 +1,24 @@
+// App.jsx
+import React from "react";
 import { Router } from "./Router";
 import { ReactComponent as BackgroundLogo } from "./components/svg/6TM.svg";
 import { ReactComponent as FuseeLogo } from "./components/svg/Fusee.svg";
+import "./App.css"; // Import your main stylesheet
 
 function App() {
-    return (
-        <div className="
-            App 
-            bg-background
-            min-w-screen min-h-screen
-            
-        ">
-            <div>
-                <div className="fixed right-0 top-[-25px] overflow-x-hidden">
-                    <BackgroundLogo />
-                </div>
-                <div className="fixed right-[1%] top-[10%] overflow-x-hidden">
-                    <FuseeLogo />
-                </div>
-            </div>
-            <Router />
-        </div >
-    );
+  return (
+    <div className="App bg-background min-w-screen min-h-screen">
+      <div>
+        <div className="fixed right-0 top-[-25px] overflow-x-hidden">
+          <BackgroundLogo />
+        </div>
+        <div className="fixed right-[1%] top-[10%] overflow-x-hidden">
+          <FuseeLogo />
+        </div>
+      </div>
+      <Router />
+    </div>
+  );
 }
 
 export default App;
