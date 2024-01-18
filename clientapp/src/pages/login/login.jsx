bb190df941ecef7ceca30146666c0bd14b2eb674
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-01-18 10:12:54 +0100

    fix(clientapp): login

--- clientapp/src/pages/login/login.jsx
+++ clientapp/src/pages/login/login.jsx
@@ -1,58 +1,98 @@
-// Login.jsx
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Header from './../../components/Header'; // Update the path based on your project structure
-import './login.css';
+import Header from './../../components/Header';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const port = process.env.PORT || 8080;
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // You can implement your login logic here
-    console.log(`Login with ${username} and ${password}`);
+    fetch(`http://localhost:${port}/api/v1/user/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
-    <div className="login-page">
+    <>
       <Header />
 
-      <div className="login-container">
-        <div className="login-box">
-          <h2>Login</h2>
-          <form>
-            <div className="form-group">
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="white-input"
-                placeholder="Nom d'utilisateur"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="white-input"
-                placeholder="Mot de passe"
-              />
-            </div>
-            <Link to="/forgot-password" className="forgot-password-link">
-              Utilisateur ou mot de passe oublié
-            </Link>
-            <br />
-            <br />
-            <button type="button" onClick={handleLogin} className="login-button">
-              Se connecter
-            </button>
-          </form>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold mb-10 text-center mt-28 text-white">Barbara</h1>
+
+        <div className="flex flex-col items-center justify-center">
+
+          <TextField
+            className="w-full"
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            sx={{
+              '& .MuiInputBase-root': { color: '#D0FFE9', backgroundColor: '#2D2D2D' },
+              '& label.Mui-focused': { color: '#D0FFE9' },
+              '& label': { color: '#D0FFE9' },
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': { borderColor: '#D0FFE9' },
+                '&:hover fieldset': { borderColor: '#D0FFE9' },
+                '&.Mui-focused fieldset': { borderColor: '#D0FFE9' }
+              },
+              margin: '8px',
+            }}
+          />
+
+          <TextField
+            className="w-full mt-5"
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            sx={{
+              '& .MuiInputBase-root': { color: '#D0FFE9', backgroundColor: '#2D2D2D' },
+              '& label.Mui-focused': { color: '#D0FFE9' },
+              '& label': { color: '#D0FFE9' },
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': { borderColor: '#D0FFE9' },
+                '&:hover fieldset': { borderColor: '#D0FFE9' },
+                '&.Mui-focused fieldset': { borderColor: '#D0FFE9' }
+              },
+              margin: '8px',
+              marginBottom: '20px',
+            }}
+          />
+
+          <Button
+            className="mt-5 w-full"
+            variant="contained"
+            onClick={handleLogin}
+            sx={{
+              backgroundColor: '#4CAF50',
+              color: 'white',
+              '&:hover': {
+                backgroundColor: '#4CAF50',
+                opacity: 0.8,
+              },
+            }}
+          >
+            Login
+          </Button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
