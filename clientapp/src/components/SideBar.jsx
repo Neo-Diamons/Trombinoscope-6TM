d8c8a4a9ecaaaa092771ea6641d072988819e5f7
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-01-18 09:12:37 +0100

    fix(clientapp/sidebar): arrow color white

--- clientapp/src/components/SideBar.jsx
+++ clientapp/src/components/SideBar.jsx
@@ -41,7 +41,7 @@ const Accordion = styled((props) => (
 
 const AccordionSummary = styled((props) => (
   <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem', color: '#D0FFE9' }} />}
     {...props}
   />
 ))(({ theme }) => ({
